3d95c8c915b6a2fa1830ab14798652858e896994
amarnath <avadla@gemini-us.com>
2019-09-25 16:36:03 +0530

    add item and transitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AppNavBar from './components/AppNavBar'
+import ShoppingList from './components/ShoppinList'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return <div className="App">
       <AppNavBar />
+      <ShoppingList/>
     </div>
   }
 
